28dc1fcccbec6a9d4c4309aaddc781cd0bc2e0a4
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2023-03-23 22:22:06 +0100

    handling multiple keys pressed

--- src/movement/MovementHandler.ts
+++ src/movement/MovementHandler.ts
@@ -3,8 +3,11 @@ import { DEFAULT_KEY_MAPPING, getMovementType, MovementType } from "./defaultKey
 import { Movable } from "./Movable";
 
 export class MovementHandler {
-  private pressedKeyY: string | null = null;
-  private pressedKeyX: string | null = null;
+  private pressedKeyY: string | undefined;
+  private pressedKeyX: string | undefined;
+  private allPressedKeysX: string[] = [];
+  private allPressedKeysY: string[] = [];
+
   constructor(private entity: Movable) {
     if (this.entity instanceof Player) {
       this.setupListener();
@@ -54,17 +57,21 @@ export class MovementHandler {
   private setupListener() {
     window.addEventListener('keydown', (e: KeyboardEvent) => {
       if (getMovementType(e.key) === MovementType.HORIZONTAL) {
+        this.allPressedKeysX.push(e.key);
         this.pressedKeyX = e.key;
       } else if (getMovementType(e.key) === MovementType.VERTICAL) {
+        this.allPressedKeysY.push(e.key);
         this.pressedKeyY = e.key;
       }
     });
 
     window.addEventListener('keyup', (e: KeyboardEvent) => {
       if (getMovementType(e.key) === MovementType.HORIZONTAL) {
-        this.pressedKeyX = null;
+        this.allPressedKeysX = this.allPressedKeysX.filter((key) => key !== e.key);
+        this.pressedKeyX = this.allPressedKeysX[this.allPressedKeysX.length - 1];
       } else if (getMovementType(e.key) === MovementType.VERTICAL) {
-        this.pressedKeyY = null;
+        this.allPressedKeysY = this.allPressedKeysY.filter((key) => key !== e.key);
+        this.pressedKeyY = this.allPressedKeysY[this.allPressedKeysY.length - 1];
       }
     });
   }
