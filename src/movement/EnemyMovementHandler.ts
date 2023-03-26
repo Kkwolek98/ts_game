8764786a74bb53b85ad91fed2d6280d901ce9ba7
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2023-03-26 18:40:50 +0200

    enemy movement handler detection radius

--- src/movement/EnemyMovementHandler.ts
+++ src/movement/EnemyMovementHandler.ts
@@ -4,12 +4,21 @@ import { angleToVector } from '../misc/consts/angleUtils';
 import { MovementHandler } from './MovementHandler';
 
 export class EnemyMovementHandler extends MovementHandler {
+
+  private detectionRadius = 450;
+
   constructor(entity: Enemy, game: Game) {
     super(entity, game);
   }
 
   public override move(): void {
-    this.followPlayer();
+    const distanceToPlayer = this.getDistanceToPlayer();
+    const isTouchingPlayer = distanceToPlayer >= (this.game.player.collision.radius + this.entity.collision.radius);
+    const isInDetectionRadius = distanceToPlayer <= this.detectionRadius;
+
+    if (isTouchingPlayer && isInDetectionRadius) {
+      this.followPlayer();
+    }
   }
 
   private followPlayer(): void {
@@ -22,5 +31,14 @@ export class EnemyMovementHandler extends MovementHandler {
     const movementVector = this.getPositionDelta(vector);
     this.entity.collision.x += movementVector.x;
     this.entity.collision.y += movementVector.y;
+  }
+
+  private getDistanceToPlayer(): number {
+    const distance = Math.hypot(
+      Math.abs(this.game?.player.collision.x - this.entity.collision.x),
+      Math.abs(this.game?.player.collision.y - this.entity.collision.y)
+    );
+
+    return distance;
   }
 }
