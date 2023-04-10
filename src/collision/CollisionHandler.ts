ed413f2aa8852472bf8cf27fe91e9d8ccb1a3443
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2023-04-10 19:42:14 +0200

    collision improvements

--- src/collision/CollisionHandler.ts
+++ src/collision/CollisionHandler.ts
@@ -1,23 +1,20 @@
 import { Enemy } from "../entities/enemies/Enemy";
-import { Player } from "../entities/Player";
 import { Movable } from "../movement/interfaces/Movable.interface";
 import { Bullet } from "../weapons/Bullet";
 
 export class CollisionHandler {
   public handleCollision(entity1: Movable, entity2: Movable) {
-    const { willCollide, distance } = this.calculateCollision(entity1, entity2);
+    const { willCollide, distance, sumOfRadii } = this.calculateCollision(entity1, entity2);
     if (!willCollide) {
       entity1.willColide = false;
-      // entity2.willColide = false;
+      entity2.willColide = false;
       return;
     };
 
-    // console.log(distance)
-    // if (distance < 1 && !entity1.willColide) {
-    //   console.log(entity2.movementVector)
-    //   entity1.collision.x += entity2.movementVector.x * entity2.speed;
-    //   entity1.collision.y += entity2.movementVector.y * entity2.speed;
-    // }
+    if (distance < sumOfRadii && !entity1.willColide) {
+      entity1.collision.x += entity2.movementVector.x * entity1.maxSpeed!;
+      entity1.collision.y += entity2.movementVector.y * entity1.maxSpeed!;
+    }
 
     entity1.willColide = true;
     entity2.willColide = true;
@@ -32,16 +29,17 @@ export class CollisionHandler {
     }
   }
 
-  private calculateCollision(entity1: Movable, entity2: Movable): { willCollide: boolean, distance: number } {
-    if (entity1 instanceof Player) {
-      console.log(entity1.movementVector)
-    }
-    const distanceBetweenEntities = Math.hypot(
+  private calculateCollision(entity1: Movable, entity2: Movable): { willCollide: boolean, distance: number, sumOfRadii: number } {
+    const futureDistanceBetweenEntities = Math.hypot(
       (entity1.collision.x + entity1.speed * entity1.movementVector.x) - (entity2.collision.x + entity2.speed * entity2.movementVector.x),
       (entity1.collision.y + entity1.speed * entity1.movementVector.y) - (entity2.collision.y + entity2.speed * entity2.movementVector.y)
     );
+    const distanceBetweenEntities = Math.hypot(
+      (entity1.collision.x) - (entity2.collision.x),
+      (entity1.collision.y) - (entity2.collision.y)
+    );
     const sumOfRadii = entity1.collision.radius + entity2.collision.radius;
 
-    return { willCollide: distanceBetweenEntities < sumOfRadii, distance: distanceBetweenEntities };
+    return { willCollide: futureDistanceBetweenEntities < sumOfRadii, distance: distanceBetweenEntities, sumOfRadii };
   }
 }
